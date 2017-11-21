7ac22472ca215a90b39349bb3b492f176217d0bb
Gokmen Goksel <gokmen@goksel.me>
2017-11-20 17:10:18 -0800

    Server: share connectedFrom info with identified data

--- src/server.js
+++ src/server.js
@@ -51,11 +51,11 @@ export default class Server extends KiteServer {
 
   registerConnection(connection) {
     const headers = connection.connection.headers || {}
-    const remoteIp =
+    const connectedFrom =
       headers['x-forwarded-for'] || connection.connection.remoteAddress
 
-    if (this.ctx.blackList.has(remoteIp)) {
-      this.logger.debug('connection request from blacklisted ip', remoteIp)
+    if (this.ctx.blackList.has(connectedFrom)) {
+      this.logger.debug('connection request from blacklisted ip', connectedFrom)
       connection.close()
       return
     }
@@ -72,7 +72,8 @@ export default class Server extends KiteServer {
 
         this.logger.info('A new kite registered with ID of', kiteId)
 
-        const identifyData = { id: kiteId }
+        const identifyData = { id: kiteId, connectedFrom }
+
         if (kiteInfo.environment == 'Browser' && useragent) {
           let { browser } = uaParser(useragent)
           let environment = `${browser.name} ${browser.version}`
@@ -103,13 +104,15 @@ export default class Server extends KiteServer {
       })
       .catch(err => {
         this.logger.error('Error while register connection', err)
-        this.logger.info('Dropping outdated kite', connectionId, remoteIp)
-        this.ctx.blackListCandidates[remoteIp] |= 0
-        this.ctx.blackListCandidates[remoteIp]++
-        if (this.ctx.blackListCandidates[remoteIp] > BLACKLIST_LIMIT) {
-          this.logger.info(`Connections from ${remoteIp} blacklisted`)
-          this.ctx.blackList.add(remoteIp)
+        this.logger.info('Dropping outdated kite', connectionId, connectedFrom)
+        this.ctx.blackListCandidates[connectedFrom] |= 0
+        this.ctx.blackListCandidates[connectedFrom]++
+
+        if (this.ctx.blackListCandidates[connectedFrom] > BLACKLIST_LIMIT) {
+          this.logger.info(`Connections from ${connectedFrom} blacklisted`)
+          this.ctx.blackList.add(connectedFrom)
         }
+
         connection.close()
       })
   }
